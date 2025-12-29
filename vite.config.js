2b686230c0a546191bc49d7ff1804c8cf0ed1a2b
Mira <myrosvas@gmail.com>
2025-12-29 19:44:59 +0100

    base updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/yoga/' : '/',
+  base: '/',
   server: {
     port: 3000,
     headers: {
